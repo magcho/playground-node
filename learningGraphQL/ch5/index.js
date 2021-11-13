0a08d90b72cdbb4dea9bb6d5233f68d1c300ad64
magcho <mail@magcho.com>
2021-11-13 21:23:05 +0900

    Init mongodb + express

--- learningGraphQL/ch5/index.js
+++ learningGraphQL/ch5/index.js
@@ -1,118 +1,36 @@
-import { ApolloServer } from "apollo-server";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 import { importSchema } from "graphql-import";
-import { GraphQLScalarType } from "graphql";
+import expressPlaygroundMiddleware from "graphql-playground-middleware-express";
+import { resolvers } from "./resolver.js";
+
+import { MongoClient } from "mongodb";
 
 const typeDefs = importSchema("./schema.graphql");
 
-const users = [
-  { githubLogin: "@magcho", name: "magcho" },
-  { githubLogin: "@magchoa", name: "magchoa" },
-  { githubLogin: "@magchob", name: "magchob" },
-];
+const app = express();
 
-let _id = 4;
-const photos = [
-  {
-    id: "1",
-    name: "magcho",
-    description: "aaaa",
-    category: "PORTRAT",
-    githubUser: "@magcho",
-    created: "2021/11/13",
-  },
-  {
-    id: "2",
-    name: "magcho",
-    description: "aaaa",
-    category: "PORTRAT",
-    githubUser: "@magcho",
-    created: "2021/11/13",
-  },
-  {
-    id: "3",
-    name: "magchob",
-    description: "aaaa",
-    category: "PORTRAT",
-    githubUser: "@magchob",
-    created: "2021/11/13",
-  },
-];
+async function startServer() {
+  const MONGO_DB = "mongodb://root:example@localhost:27017/";
+  const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true });
+  const db = client.db();
+  const context = { db };
 
-const tags = [
-  {
-    photoId: "1",
-    userId: "@magchoa",
-  },
-  {
-    photoId: "2",
-    userId: "@magcho",
-  },
-  {
-    photoId: "2",
-    userId: "@magchoa",
-  },
-  {
-    photoId: "2",
-    userId: "@magchob",
-  },
-];
+  const server = new ApolloServer({ typeDefs, resolvers, context });
+  await server.start();
+  server.applyMiddleware({ app });
 
-const resolvers = {
-  Query: {
-    totalPhotos: () => photos.length,
-    allPhotos: (parent, args) => {
-      return photos;
-    },
-    allUsers: () => users,
-  },
-  Mutation: {
-    postPhoto(parent, args) {
-      const newPhoto = {
-        id: _id++,
-        ...args.input,
-        created: new Date(),
-      };
-      photos.push(newPhoto);
-      return newPhoto;
-    },
-  },
-  Photo: {
-    url: (parent) => `http://example.com/${parent.id}.jpg`,
-    postedBy: (parent) => {
-      return users.filter((user) => user.githubLogin === parent.githubUser);
-    },
-    taggedUsers: (parent) => {
-      return tags
-        .filter((tag) => tag.photoId === parent.id)
-        .map((tag) => tag.userId)
-        .map((userid) => users.find((user) => user.githubLogin === userid));
-    },
-  },
-  User: {
-    postedPhotos: (parent) => {
-      return photos.filter((photo) => photo.githubUser === parent.githubLogin);
-    },
-    inPhotos: (parent) => {
-      return tags
-        .filter((tag) => tag.userId === parent.githubLogin)
-        .map((tag) => tag.photoId)
-        .map((photoId) => photos.find((photo) => photo.id === photoId));
-    },
-  },
-  DateTime: new GraphQLScalarType({
-    name: "DateTime",
-    description: "A valid date time value",
-    parseValue: (value) => new Date(value),
-    serialize: (value) => new Date(value).toISOString(),
-    parseLiteral: (ast) => ast.value,
-  }),
-};
+  app.listen({ port: 4000 }, () => {
+    console.log(`start server http://localhost:4000${server.graphqlPath}`);
+  });
+}
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+startServer();
+app.get("/", (_, res) => {
+  res.end("hello");
 });
 
-server.listen().then(({ url }) => {
-  console.log(`GraphQL service running on ${url}`);
-});
+app.get(
+  "/playground",
+  expressPlaygroundMiddleware.default({ endpoint: "/graphql" })
+);
---
+++ learningGraphQL/ch5/resolver.js
@@ -0,0 +1,104 @@
+import { GraphQLScalarType } from "graphql";
+const users = [
+  { githubLogin: "@magcho", name: "magcho" },
+  { githubLogin: "@magchoa", name: "magchoa" },
+  { githubLogin: "@magchob", name: "magchob" },
+];
+
+let _id = 4;
+const photos = [
+  {
+    id: "1",
+    name: "magcho",
+    description: "aaaa",
+    category: "PORTRAT",
+    githubUser: "@magcho",
+    created: "2021/11/13",
+  },
+  {
+    id: "2",
+    name: "magcho",
+    description: "aaaa",
+    category: "PORTRAT",
+    githubUser: "@magcho",
+    created: "2021/11/13",
+  },
+  {
+    id: "3",
+    name: "magchob",
+    description: "aaaa",
+    category: "PORTRAT",
+    githubUser: "@magchob",
+    created: "2021/11/13",
+  },
+];
+
+const tags = [
+  {
+    photoId: "1",
+    userId: "@magchoa",
+  },
+  {
+    photoId: "2",
+    userId: "@magcho",
+  },
+  {
+    photoId: "2",
+    userId: "@magchoa",
+  },
+  {
+    photoId: "2",
+    userId: "@magchob",
+  },
+];
+
+export const resolvers = {
+  Query: {
+    totalPhotos: () => photos.length,
+    allPhotos: (parent, args) => {
+      return photos;
+    },
+    allUsers: () => users,
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      const newPhoto = {
+        id: _id++,
+        ...args.input,
+        created: new Date(),
+      };
+      photos.push(newPhoto);
+      return newPhoto;
+    },
+  },
+  Photo: {
+    url: (parent) => `http://example.com/${parent.id}.jpg`,
+    postedBy: (parent) => {
+      return users.filter((user) => user.githubLogin === parent.githubUser);
+    },
+    taggedUsers: (parent) => {
+      return tags
+        .filter((tag) => tag.photoId === parent.id)
+        .map((tag) => tag.userId)
+        .map((userid) => users.find((user) => user.githubLogin === userid));
+    },
+  },
+  User: {
+    postedPhotos: (parent) => {
+      return photos.filter((photo) => photo.githubUser === parent.githubLogin);
+    },
+    inPhotos: (parent) => {
+      return tags
+        .filter((tag) => tag.userId === parent.githubLogin)
+        .map((tag) => tag.photoId)
+        .map((photoId) => photos.find((photo) => photo.id === photoId));
+    },
+  },
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "A valid date time value",
+    parseValue: (value) => new Date(value),
+    serialize: (value) => new Date(value).toISOString(),
+    parseLiteral: (ast) => ast.value,
+  }),
+};
