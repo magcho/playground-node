2bf100979cc1b61744b17636d9abdb4efdd0fe5d
magcho <mail@magcho.com>
2022-10-02 02:57:58 +0900

    update: store dynamodb

--- serverless-framework-aws-node-http/handler.js
+++ serverless-framework-aws-node-http/handler.js
@@ -1,4 +1,5 @@
 "use strict";
+const AWS = require("aws-sdk");
 
 module.exports.hello = async (event) => {
   return {
@@ -11,5 +12,22 @@ module.exports.hello = async (event) => {
       null,
       2
     ),
+  };
+};
+
+module.exports.createCustomer = async (event) => {
+  const body = JSON.parse(Buffer.from(event.body, "base64").toString());
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  const putParams = {
+    TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
+    Item: {
+      primary_key: body.name,
+      email: body.email,
+    },
+  };
+  await dynamoDb.put(putParams).promise();
+
+  return {
+    statusCode: 201,
   };
 };
