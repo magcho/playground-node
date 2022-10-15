8929e6371727e657e44f28fbad8237a0693e9726
magcho <mail@magcho.com>
2022-10-16 02:44:36 +0900

    theme custom

--- rapidoc-vue3/src/App.vue
+++ rapidoc-vue3/src/App.vue
@@ -3,16 +3,28 @@ import "rapidoc";
 </script>
 
 <template>
+  <header class="global"><span>hello</span></header>
   <rapi-doc
-    spec-url="https://petstore.swagger.io/v2/swagger.json"
+    spec-url="/sample.json"
     render-style="read"
-    style="height: 100vh; width: 100%"
+    show-header="false"
+    allow-spec-url-load="false"
+    allow-spec-file-load="false"
+    primary-color="#00b2db"
   >
+    <header class="internal">ここが内側のheader</header>
+    <footer>ここが内側のfooter</footer>
   </rapi-doc>
 </template>
 
-<style>
-body {
-  margin: 0;
+<style scoped>
+rapi-doc {
+  height: calc(100vh - 30px);
+  width: 100%;
+}
+header {
+  height: 30px;
+  width: 100%;
+  background: #ddffff;
 }
 </style>
