cf907cda33db22d742f86a5d9e6c2b462b85442e
magcho <mail@magcho.com>
2022-10-16 02:00:31 +0900

    rapidoc

---
+++ rapidoc-vue3/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import "rapidoc";
+</script>
+
+<template>
+  <rapi-doc
+    spec-url="https://petstore.swagger.io/v2/swagger.json"
+    render-style="read"
+    style="height: 100vh; width: 100%"
+  >
+  </rapi-doc>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
