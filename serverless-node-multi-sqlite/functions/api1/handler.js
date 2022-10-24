85ffd0859fa69eece7721c1603c9014b697bc835
magcho <mail@magcho.com>
2022-10-25 00:54:29 +0900

    sls node http sqlite

---
+++ serverless-node-multi-sqlite/functions/api1/handler.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const knex = require("knex")({
+  client: "better-sqlite3",
+  connection: {
+    filename: "./database1.sqlite",
+  },
+});
+
+module.exports.get = async (event) => {
+  const result = await knex("sample").limit(100);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+        data: result,
+      },
+      null,
+      2
+    ),
+  };
+};
