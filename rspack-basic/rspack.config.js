a10493e219b09bb950ff26bde264edde39b26617
magcho <mail@magcho.com>
2023-04-08 19:20:53 +0900

    feat: bundle analyzer setup

--- rspack-basic/rspack.config.js
+++ rspack-basic/rspack.config.js
@@ -1,24 +1,34 @@
+const { StatsWriterPlugin } = require("webpack-stats-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 /**
  * @type {import('@rspack/cli').Configuration}
  */
 module.exports = {
-	context: __dirname,
-	entry: {
-		main: "./src/main.jsx"
-	},
-	builtins: {
-		html: [
-			{
-				template: "./index.html"
-			}
-		]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svg$/,
-				type: "asset"
-			}
-		]
-	}
+  context: __dirname,
+  entry: {
+    main: "./src/main.jsx",
+  },
+  builtins: {
+    html: [
+      {
+        template: "./index.html",
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: "asset",
+      },
+    ],
+  },
+  plugins: [
+    new StatsWriterPlugin({
+      filename: "stats.json",
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
 };
