429de16b751903d920c16776e9bbf5d7c123e321
magcho <mail@magcho.com>
2024-08-11 17:39:33 +0900

    up

---
+++ serverless-framework-resource-terraform/handler.js
@@ -0,0 +1,11 @@
+exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v4! Your function executed successfully!",
+    }),
+  };
+};
+
+
+
